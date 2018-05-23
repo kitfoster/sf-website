96a54fcf946c85513521c9a49b5843fba3dd9fbf
kit <kit.foster@rea-group.com>
2018-05-23 10:58:30 +1000

    add gulpfile for scss

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var del = require('del');
+
+var sassFiles = './wp-content/themes/sinisgallifoster/sass/**/*.scss'
+var cssDest = './wp-content/themes/sinisgallifoster/assets/css/';
+var cssFiles = cssDest + 'app.css';
+
+gulp.task('delete', function () {
+  return del([cssDest + '/**/*']);
+});
+
+gulp.task("clean", function () {
+  return del("assets/css/*.cs*");
+});
+
+gulp.task('sass', function () {
+  return gulp.src(sassFiles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(cssDest));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(sassFiles, ['sass']);
+});
+
+gulp.task('autoprefixer', function () {
+    return gulp.src(cssFiles)
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(cssDest));
+});
+
+
+gulp.task('build', ['clean'], function () {
+  gulp.start( ['sass', 'autoprefixer'] );
+});
+gulp.task('default', ['clean', 'sass', 'autoprefixer']);
