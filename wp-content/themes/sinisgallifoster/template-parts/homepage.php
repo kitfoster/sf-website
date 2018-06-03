b3324818667c8c383303f1c1f0ddb920460d22b9
kit <kit.foster@rea-group.com>
2018-06-03 21:00:48 +1000

    homepage mobile

--- wp-content/themes/sinisgallifoster/template-parts/homepage.php
+++ wp-content/themes/sinisgallifoster/template-parts/homepage.php
@@ -5,4 +5,13 @@
     </h2>
     <div class="arrow-down"></div>
   </div>
+  <div class="homepage-mobile">
+    <div class="photo"></div>
+    <div class="bottom">
+      <h2>
+        Sinisgalli Foster Melbourne Lawyers dolor sit amet, no viderer aliquam vel, fastidii mentitum deseruisse ut mel. Vis nibh veri mazim no. Quo ne aliquam fabellas consetetur.
+      </h2>
+      <div class="arrow-down"></div>
+    </div>
+  </div>
 </div>
