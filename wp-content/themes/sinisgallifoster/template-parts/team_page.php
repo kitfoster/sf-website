d198c60e4d2b7ecff4cfe328763c99240fe0cc37
kit <kit.foster@rea-group.com>
2018-06-03 21:40:30 +1000

    add people carousel

--- wp-content/themes/sinisgallifoster/template-parts/team_page.php
+++ wp-content/themes/sinisgallifoster/template-parts/team_page.php
@@ -7,16 +7,47 @@
     }
   }
   $person = new Person();
+  $first = true;
 ?>
 
 
 <div class="team-page-container desktop-container">
-  <h3>Our Team</h3>
-  <?php foreach([$person, $person, $person, $person, $person, $person, $person, $person, $person] as $key=>$value): ?>
-    <div class="bio-container">
-      <img src=<?php echo $value->photo(); ?> alt="Example Lawyer Photo"></img>
-      <div class="name"><?php echo $value->name; ?></div>
-      <div class="role"><?php echo $value->role; ?></div>
+  <!-- desktop -->
+  <div class="desktop-view">
+    <h3>Our Team</h3>
+    <?php foreach([$person, $person, $person, $person, $person, $person, $person, $person, $person] as $key=>$value): ?>
+      <div class="bio-container">
+        <img src=<?php echo $value->photo(); ?> alt="Example Lawyer Photo"></img>
+        <div class="name"><?php echo $value->name; ?></div>
+        <div class="role"><?php echo $value->role; ?></div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+
+  <!-- mobile -->
+  <div class="mobile-carousel">
+    <h3>Our Team</h3>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <?php foreach([$person, $person, $person, $person, $person, $person, $person, $person, $person] as $key=>$value): ?>
+          <div class="bio-container item <?php if ($first) {echo "active"; $first = false;} ?>">
+            <img src=<?php echo $value->photo(); ?> alt="Example Lawyer Photo"></img>
+            <div class="name"><?php echo $value->name; ?></div>
+            <div class="role"><?php echo $value->role; ?></div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
-  <?php endforeach; ?>
+  </div>
 </div>
