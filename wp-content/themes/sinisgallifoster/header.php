90977bdc689ebee1626a5c61ad02327323c6914a
kit <kit.foster@rea-group.com>
2018-06-04 10:04:38 +1000

    carousel without bootsrap

--- wp-content/themes/sinisgallifoster/header.php
+++ wp-content/themes/sinisgallifoster/header.php
@@ -14,9 +14,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link href="https://fonts.googleapis.com/css?family=Lato:300,400|Lora" rel="stylesheet">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <!-- for testing in IE vm - 10.0.2.2:8080/-->
   <link rel="stylesheet" type="text/css" href="wp-content/themes/sinisgallifoster/style.css">
 
--- wp-content/themes/sinisgallifoster/template-parts/team_page.php
+++ wp-content/themes/sinisgallifoster/template-parts/team_page.php
@@ -12,42 +12,49 @@
 
 
 <div class="team-page-container desktop-container">
+  <h3>Our Team</h3>
+
   <!-- desktop -->
   <div class="desktop-view">
-    <h3>Our Team</h3>
     <?php foreach([$person, $person, $person, $person, $person, $person, $person, $person, $person] as $key=>$value): ?>
       <div class="bio-container">
         <img src=<?php echo $value->photo(); ?> alt="Example Lawyer Photo"></img>
-        <div class="name"><?php echo $value->name; ?></div>
-        <div class="role"><?php echo $value->role; ?></div>
+        <span class="name"><?php echo $value->name; ?></span>
+        <span class="role"><?php echo $value->role; ?></span>
       </div>
     <?php endforeach; ?>
   </div>
 
   <!-- mobile -->
   <div class="mobile-carousel">
-    <h3>Our Team</h3>
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <?php foreach([$person, $person, $person, $person, $person, $person, $person, $person, $person] as $key=>$value): ?>
-          <div class="bio-container item <?php if ($first) {echo "active"; $first = false;} ?>">
-            <img src=<?php echo $value->photo(); ?> alt="Example Lawyer Photo"></img>
-            <div class="name"><?php echo $value->name; ?></div>
-            <div class="role"><?php echo $value->role; ?></div>
-          </div>
-        <?php endforeach; ?>
+    <?php foreach([$person, $person, $person, $person, $person, $person, $person, $person, $person] as $key=>$value): ?>
+      <div class="carousel-slides">
+        <img src=<?php echo $value->photo(); ?> alt="Example Lawyer Photo"></img>
+        <span class="name"><?php echo $value->name; ?></span>
+        <span class="role"><?php echo $value->role; ?></span>
       </div>
-
-      <!-- Left and right controls -->
-      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
+    <?php endforeach; ?>
+    <button class="carousel-button left" onclick="plusDivs(-1)"></button>
+    <button class="carousel-button right" onclick="plusDivs(1)"></button>
   </div>
 </div>
+
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("carousel-slides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+</script>
