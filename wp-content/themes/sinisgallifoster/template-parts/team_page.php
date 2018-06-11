984c3ee58aa11786987b790088a7474eff5558d2
kit <kit.foster@rea-group.com>
2018-06-11 11:08:05 +1000

    update team page functions

--- wp-content/themes/sinisgallifoster/template-parts/team_page.php
+++ wp-content/themes/sinisgallifoster/template-parts/team_page.php
@@ -86,36 +86,14 @@ function showDivs(n) {
 function toggleBio(index) {
   var x = document.getElementById(`bio-expanded${index}`);
   if (x.className == "bio-expanded") {
-    console.log('heyeyeyye');
+    x.style.opacity = "1";
     x.className += " open";
   }
   else {
     x.style.opacity = "0";
     setTimeout(function() {
-      closeAllBios();
+      x.className = "bio-expanded";
     }, 500);
   }
 }
-
-function closeAllBios() {
-  var x = document.getElementsByClassName('bio-expanded open');
-  console.log(x);
-  for (i = 0; i < x.length; i++) {
-    x[i].className == "bio-expanded";
-  }
-}
-
-// close the bio when user presses escape
-document.onkeydown = function(evt) {
-    evt = evt || window.event;
-    var isEscape = false;
-    if ("key" in evt) {
-        isEscape = (evt.key == "Escape" || evt.key == "Esc");
-    } else {
-        isEscape = (evt.keyCode == 27);
-    }
-    if (isEscape) {
-        closeAllBios();
-    }
-};
 </script>
