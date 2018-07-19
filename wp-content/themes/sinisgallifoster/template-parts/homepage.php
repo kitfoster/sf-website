a5a7a032e1e177ca399b0ee063cf4165609d12f7
kit <kit.foster@rea-group.com>
2018-07-19 13:29:23 +1000

    Update homepage styling

--- wp-content/themes/sinisgallifoster/template-parts/homepage.php
+++ wp-content/themes/sinisgallifoster/template-parts/homepage.php
@@ -1,17 +1,14 @@
+<?php
+  $homepageParagraph = get_field("homepage_text");
+  $homepageImage = get_field("homepage_image");
+?>
+
 <div class="homepage-background" id="about">
-  <div class="homepage-container desktop-container" >
+  <img src=<?php echo $homepageImage?>></img>
+  <div class="homepage-container desktop-container">
     <h2>
       <?php echo $homepageParagraph ?>
     </h2>
     <a class="arrow-down" onclick="scrollToElement('statement')"></a>
-  </div>
-  <div class="homepage-mobile">
-    <div class="photo"></div>
-    <div class="bottom">
-      <h2>
-        <?php echo $homepageParagraph ?>
-      </h2>
-      <a class="arrow-down" onclick="scrollToElement('statement')"></a>
-    </div>
   </div>
 </div>
