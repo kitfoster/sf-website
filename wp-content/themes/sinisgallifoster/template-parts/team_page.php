0ce2775632f3d85750568c16edfe7c8986dbbd65
kit <kit.foster@rea-group.com>
2018-07-19 11:26:24 +1000

    Update team_page to connect to WP

--- wp-content/themes/sinisgallifoster/template-parts/team_page.php
+++ wp-content/themes/sinisgallifoster/template-parts/team_page.php
@@ -3,29 +3,47 @@
     public $name;
     public $role;
     public $photo;
-    public $accreditations = "Accredited Commmercial</br>Litigation Specialist";
-    public $bio = "Lorem Ipsor Dolem imet dolor sit amet, no viderer aliquam vel, fastidii mentitum deseruisse ut mel. Vis nibh veri mazim no. Quo ne aliquam fabellas consetetur.Lorem Ipsor Dolem imet dolor sit amet, no viderer aliquam vel, fastidii mentitum deseruisse ut mel. Vis nibh veri mazim no. Quo ne aliquam fabellas consetetur.Lorem Ipsor Dolem imet dolor sit amet, no viderer aliquam vel, fastidii mentitum deseruisse ut mel.</br></br> Vis nibh veri mazim no. Quo ne aliquam fabellas consetetur.Lorem Ipsor Dolem imet dolor sit amet, no viderer aliquam vel, fastidii mentitum deseruisse ut mel. Vis nibh veri mazim no. Quo ne aliquam fabellas consetetur.Lorem Ipsor Dolem imet dolor sit amet, no viderer aliquam vel, fastidii mentitum deseruisse ut mel. Vis nibh veri mazim no. Quo ne aliquam fabellas consetetur.";
-    public $mobile = "0444 444 444";
-    public $email = "first@sinisgallifoster.com.au";
-    public $linkedIn = "https://www.linkedin.com";
+    public $accreditations;
+    public $bio;
+    public $contact_number;
+    public $email;
+    public $linked_in;
 
-    public function __construct($name, $role, $photo) {
+    public function __construct($name, $role, $photo, $accreditations, $bio, $contact_number, $email, $linked_in) {
       $this->name = $name;
       $this->role = $role;
       $this->photo = $photo;
+      $this->accreditations = $accreditations;
+      $this->bio = $bio;
+      $this->contact_number = $contact_number;
+      $this->email = $email;
+      $this->linked_in = $linked_in;
     }
   }
-  $person1 = new Person("first last", "role title", "wp-content/themes/sinisgallifoster/assets/images/test-portrait.jpg");
-  $person2 = new Person("first last other", "role title other", "wp-content/themes/sinisgallifoster/assets/images/corporate_portrait.jpg");
-?>
 
+  $team = array();
+  while ( have_posts() ) :
+    the_post();
+    $name = get_field("name");
+    $role = get_field("role");
+    $photo = get_field("photo");
+    $accreditations = get_field("accreditations");
+    $bio = get_field("bio");
+    $contact_number = get_field("contact_number");
+    $email = get_field("email");
+    $linked_in = get_field("linked_in");
+
+    $employee = new Person($name, $role, $photo, $accreditations, $bio, $contact_number, $email, $linked_in);
+    array_push($team, $employee);
+  endwhile;
+?>
 
 <div class="team-page-container desktop-container" id="our_team">
   <h3>Our Team</h3>
 
   <!-- desktop -->
   <div class="desktop-view">
-    <?php foreach([$person1, $person2, $person1, $person2, $person1, $person2, $person1, $person2, $person1] as $key=>$value): ?>
+    <?php foreach($team as $key=>$value): ?>
       <div class="bio-container" id=<?php echo "bio-container" . $key; ?> onclick="toggleBio('<?php echo "bio-expanded" . $key; ?>')" onmouseleave="removeImageEffects()">
         <img class="bio-image" id="<?php echo "bio-image" . $key; ?>" onmouseover="imageEffect('<?php echo "bio-image" . $key; ?>')" src=<?php echo $value->photo; ?> alt="Example Lawyer Photo"></img>
         <span class="name"><?php echo $value->name; ?></span>
@@ -36,7 +54,7 @@
 
   <!-- mobile -->
   <div class="mobile-carousel">
-    <?php foreach([$person1, $person2, $person1, $person2, $person1, $person2, $person1, $person2, $person1] as $key=>$value): ?>
+    <?php foreach($team as $key=>$value): ?>
     <div class="carousel-slides" id=<?php echo "carousel-slides" . $key; ?> onclick="toggleBio('<?php echo "bio-expanded" . $key; ?>')">
       <img src=<?php echo $value->photo; ?> alt="Example Lawyer Photo"></img>
       <span class="name"><?php echo $value->name; ?></span>
@@ -48,7 +66,7 @@
   </div>
 
   <!-- bios -->
-  <?php foreach([$person1, $person2, $person1, $person2, $person1, $person2, $person1, $person2, $person1] as $key=>$value): ?>
+  <?php foreach($team as $key=>$value): ?>
     <div class="bio-expanded" id=<?php echo "bio-expanded" . $key; ?>>
       <div class="bio-inner">
         <button class="close-bio" onclick="toggleBio('<?php echo "bio-expanded" . $key; ?>')"></button>
@@ -56,9 +74,9 @@
         <span class="role"><?php echo $value->role; ?></span>
         <span class="accreditations"><?php echo $value->accreditations; ?></span>
         <span class="bio"><?php echo $value->bio; ?></span>
-        <span class="mobile">M / <?php echo $value->mobile; ?></span>
+        <span class="contact_number">P / <?php echo $value->contact_number; ?></span>
         <span class="email">E / <?php echo $value->email; ?></span>
-        <a class="linkedIn" href=<?php echo $value->linkedIn; ?>><img src="wp-content/themes/sinisgallifoster/assets/images/linkedin.svg" alt="LinkedIn Logo" />visit <?php echo $value->name; ?>'s LinkedIn</a>
+        <a class="linkedIn" href=<?php echo $value->linkedIn; ?>><img src="wp-content/themes/sinisgallifoster/assets/images/linkedin.svg" alt="LinkedIn Logo" />Visit <?php echo $value->name; ?>'s LinkedIn</a>
       </div>
     </div>
   <?php endforeach; ?>
