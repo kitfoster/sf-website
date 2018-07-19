6a0c69a67b40c96d947e965c970afd380f1861c5
kit <kit.foster@rea-group.com>
2018-07-19 13:29:13 +1000

    Moving logic and images to WP

--- wp-content/themes/sinisgallifoster/details.php
+++ wp-content/themes/sinisgallifoster/details.php
@@ -4,5 +4,4 @@
   $addressLine1 = "Level 10 / 333 Collins Street,";
   $addressLine2 = "Melbourne CBD, VIC 3000";
   $copy = "&copy; Sinisgalli Foster 2018";
-  $homepageParagraph = "Sinisgalli Foster Melbourne Lawyers dolor sit amet, no viderer aliquam vel, fastidii mentitum deseruisse ut mel. Vis nibh veri mazim no. Quo ne aliquam fabellas consetetur.";
 ?>
