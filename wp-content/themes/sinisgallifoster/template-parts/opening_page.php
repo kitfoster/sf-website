e57e5f802809d427600390d7085778b80cb7e89b
kit <kit.foster@rea-group.com>
2018-08-07 09:29:47 +1000

    fix ie loading bug

--- wp-content/themes/sinisgallifoster/template-parts/opening_page.php
+++ wp-content/themes/sinisgallifoster/template-parts/opening_page.php
@@ -2,13 +2,16 @@
   <img src="wp-content/themes/sinisgallifoster/assets/images/sf-logo.gif" alt="Sinisgalli Foster Logo"/>
 </div>
 
+
 <script>
-window.onload = function(e) {
+
+var windowOnload = window.onload || function() {
   var page = document.getElementById("opening-page");
   page.className += " fade-out"
-  setTimeout(() => {
+  setTimeout(function() {
     page.style.display = "none";
   }, 1000);
-}
+};
+window.onload = function() {windowOnload(); };
 
 </script>
