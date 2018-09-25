0e642c0a3f41b60cca8cdaca9e4fe3ebf8eded4c
kit <katya.foster@rea-group.com>
2018-09-25 16:17:46 +1000

    better but not perfect on ie

--- wp-content/themes/sinisgallifoster/template-parts/safe_custody.php
+++ wp-content/themes/sinisgallifoster/template-parts/safe_custody.php
@@ -1,8 +1,6 @@
-<div class="safe-custody-container" id="safe-custody">
-  <div class="desktop-container">
+<div class="safe-custody-container desktop-container" id="safe-custody">
     <h3 class="safe-custody-heading">Safe Custody</h3>
     <p class="safe-custody-body"><?php echo $safeCustody->text; ?></p>
-  </div>
 </div>
 
 <script>
