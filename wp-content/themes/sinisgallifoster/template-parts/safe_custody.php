3bdf02bf6b31cfbc0f858b787fdd6abb1d77c2ba
kit <kit.foster@rea-group.com>
2019-02-05 15:16:36 +1100

    updated safe custody and other small things

--- wp-content/themes/sinisgallifoster/template-parts/safe_custody.php
+++ wp-content/themes/sinisgallifoster/template-parts/safe_custody.php
@@ -1,9 +1,21 @@
 <div class="safe-custody-container desktop-container" id="safe-custody">
-    <h3 class="safe-custody-heading">Safe Custody</h3>
-    <p class="safe-custody-body"><?php echo $safeCustody->text; ?></p>
+  <div class="heading_wrapper">
+    <h3 class="safe-custody-heading" id="safe-custody-heading" onclick="safeCustodyToggle()">Safe Custody</h3>
+    <a class="arrow-down" onclick="safeCustodyToggle()"></a>
+  </div>
+  <p class="safe-custody-body" onclick="safeCustodyToggle()"><?php echo $safeCustody->text; ?></p>
 </div>
 
 <script>
-  sr.reveal('.safe-custody-heading', {distance: '50px'});
-  sr.reveal('.safe-custody-body', {distance: '50px'});
+  sr.reveal('.heading_wrapper', {distance: '50px'});
+
+  function safeCustodyToggle() {
+    var sc_div = document.getElementById("safe-custody");
+    var sc_heading = document.getElementById("safe-custody-heading");
+    if (sc_div.className.includes("open")) {
+      sc_div.className = "safe-custody-container desktop-container";
+    } else {
+      sc_div.className = "safe-custody-container desktop-container open";
+    }
+  }
 </script>
