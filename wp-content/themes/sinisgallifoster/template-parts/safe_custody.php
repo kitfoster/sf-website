a7096ddaabfa43ba099ec3dc2d5c8a9012a05983
kit <kit.foster@rea-group.com>
2020-08-13 12:19:43 +1000

    Update safe custody style

--- wp-content/themes/sinisgallifoster/template-parts/safe_custody.php
+++ wp-content/themes/sinisgallifoster/template-parts/safe_custody.php
@@ -1,19 +1,6 @@
-<div class="safe-custody-container desktop-container" id="safe_custody">
-  <div class="heading_wrapper" onclick="safeCustodyToggle()">
+<div class="safe-custody-container" id="safe_custody">
+  <div class="safe-custody">
     <h3 class="safe-custody-heading" id="safe-custody-heading">Safe Custody</h3>
-    <a class="arrow-down"></a>
+    <p class="safe-custody-body"><?php echo $safeCustody->text; ?></p>
   </div>
-  <p class="safe-custody-body" onclick="safeCustodyToggle()"><?php echo $safeCustody->text; ?></p>
 </div>
-
-<script>
-  function safeCustodyToggle() {
-    var sc_div = document.getElementById("safe_custody");
-    var sc_heading = document.getElementById("safe-custody-heading");
-    if (sc_div.className.includes("open")) {
-      sc_div.className = "safe-custody-container desktop-container";
-    } else {
-      sc_div.className = "safe-custody-container desktop-container open";
-    }
-  }
-</script>
